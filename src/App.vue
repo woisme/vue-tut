c12d72b052bd7ac9513341bf64b258f644e74ccc
woisme <mywoisme@gmail.com>
2018-06-21 11:27:19 +0100

    new folder code

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Hello Worl"/>
-  </div>
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+        <div class="row">
+            <servers></servers>
+            <app-server-details></app-server-details>
+        </div>
+        <hr>
+        <app-footer></app-footer>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Header from './components/Shared/Header.vue';
+    import Footer from './components/Shared/Footer.vue';
+    import Servers from './components/Server/Servers.vue';
+    import ServerDetails from './components/Server/ServerDetails.vue';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        components: {
+            appHeader: Header,
+            Servers,
+            'app-server-details': ServerDetails,
+            'app-footer': Footer
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/ServerStatus.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <p>Service Status: {{ status }}</p>
+  <hr>
+  <button @click="changeStatus">Change Status</button>
+</div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        status: 'Critical'
+      }
+    },
+    methods: {
+      changeStatus() {
+        this.status = 'Normal'
+      }
+    }
+  }
+  
+</script>
+
